7f96a3d6cae16a1b5a522652e4c82fe0ba8f3f55
jason <jasonyang.final@gmail.com>
2018-08-22 06:48:22 +0900

    update

--- docker/docker.go
+++ docker/docker.go
@@ -1,56 +1,92 @@
 package docker
 
 import (
-	"bufio"
-	"context"
-	"encoding/base64"
+	b64 "encoding/base64"
 	"encoding/json"
 	"fmt"
-	"io"
-	"os"
-
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/client"
 	. "github.com/jasonyangshadow/lpmx/error"
-	. "github.com/jasonyangshadow/lpmx/log"
+	"io/ioutil"
+	"net/http"
+	"time"
 )
 
-//private funcs
-func pullImage(name string, output string, auth bool) *Error {
-	ctx := context.Background()
-	cli, err := client.NewEnvClient()
-	if err != nil {
-		cerr := ErrNew(err, "pullImage create env client encounters error")
-		return cerr
-	}
-	if auth {
-		scanner := bufio.NewScanner(os.Stdin)
-		fmt.Println("Enter your username")
-		user := scanner.Text()
-		fmt.Println("Enter your password")
-		pwd := scanner.Text()
-		authConfig := types.AuthConfig{
-			Username: user,
-			Password: pwd,
-		}
-		encodedJSON, err := json.Marshal(authConfig)
-		if err != nil {
-			cerr := ErrNew(err, "pullImage json.Marshal encounters error")
-			return cerr
-		}
-		authStr := base64.URLEncoding.EncodeToString(encodedJSON)
-		out, err := cli.ImagePull(ctx, name, types.ImagePullOptions{RegistryAuth: authStr})
-	} else {
-		out, err := cli.ImagePull(ctx, name, types.ImagePullOptions{})
-	}
-	if err != nil {
-		cerr := ErrNew(err, "pullImage image pull encounters error")
-		return cerr
-	}
-	defer out.close()
+const (
+	DOCKER_HUB_TOKEN = "https://auth.docker.io/token?serivce=registry.docker.io&scope="
+	CATALOG_LIMIT    = 50
+)
 
-	io.copy(output, out)
+func createTransport() *http.Transport {
+	tr := &http.Transport{
+		MaxIdleConns:       10,
+		IdleConnTimeout:    30 * time.Second,
+		DisableCompression: true,
+	}
+	return tr
 }
 
-func PullImage(name string) *Error {
+func httpExCode(r *http.Response) (int, *Error) {
+	switch r.StatusCode {
+	case 200:
+		return 200, nil
+	case 401:
+		cerr := ErrNew(ErrHttpUnauthorized, "401 unauthorized")
+		return 401, cerr
+	case 404:
+		cerr := ErrNew(ErrHttpNotFound, "404 not found")
+		return 404, cerr
+	default:
+		cerr := ErrNew(ErrUnknown, "http exception unknown")
+		return -1, cerr
+	}
+}
+
+var http_client = &http.Client{Transport: createTransport()}
+
+//return params are: successful(bool), statuscode, token, refresh_token, err(if it has)
+func RegistryAuthenticate(target string, operations string) (bool, int, string, *Error) {
+	requrl := DOCKER_HUB_TOKEN + "repository:" + target + ":" + operations
+	resp, err := http_client.Get(requrl)
+	defer resp.Body.Close()
+	if err != nil {
+		cerr := ErrNew(err, "http get encountered failure")
+		bodyBytes, _ := ioutil.ReadAll(resp.Body)
+		return false, -1, string(bodyBytes), cerr
+	}
+	retcode, cerr := httpExCode(resp)
+	if retcode == 200 && cerr == nil {
+		bodyBytes, _ := ioutil.ReadAll(resp.Body)
+		data := make(map[string]interface{})
+		if err := json.Unmarshal(bodyBytes, &data); err != nil {
+			cerr := ErrNew(err, "json unmarshal http body failure")
+			return false, -1, "", cerr
+		}
+		return true, 200, data["token"].(string), nil
+	} else {
+		cerr := ErrNew(err, "http get encountered failure")
+		bodyBytes, _ := ioutil.ReadAll(resp.Body)
+		return false, resp.StatusCode, string(bodyBytes), cerr
+	}
+}
+
+func RegistryAuthenticateBasic(target string, operations string, user string, pass string) (bool, int, string, *Error) {
+	requrl := DOCKER_HUB_TOKEN + "repository:" + target + ":" + operations
+	req, _ := http.NewRequest("GET", requrl, nil)
+	auth := b64.StdEncoding.EncodeToString([]byte(user + ":" + pass))
+	req.Header.Add("Authorization", auth)
+	resp, err := http_client.Do(req)
+	defer resp.Body.Close()
+	if err != nil {
+		cerr := ErrNew(err, "http get encountered failure")
+		bodyBytes, _ := ioutil.ReadAll(resp.Body)
+		return false, -1, string(bodyBytes), cerr
+	}
+	retcode, cerr := httpExCode(resp)
+	if retcode == 200 && cerr == nil {
+		return true, 200, "token", nil
+	} else {
+		cerr := ErrNew(err, "http get encountered failure")
+		fmt.Println(resp)
+		bodyBytes, _ := ioutil.ReadAll(resp.Body)
+		return false, resp.StatusCode, string(bodyBytes), cerr
+	}
 }
--- docker/docker_test.go
+++ docker/docker_test.go
@@ -4,5 +4,7 @@ import (
 	"testing"
 )
 
-func TestDocker(t *testing.T) {
+func TestDockerTokenBasic(t *testing.T) {
+	ret, code, token, err := RegistryAuthenticateBasic("jasonyangshadow/python", "pull", "jasonyangshadow", "jason294514")
+	t.Log(ret, code, token, err)
 }
