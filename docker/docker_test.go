13dd1636d5b2942458c5e19de49aee2b8191ec77
jason <jasonyang.final@gmail.com>
2018-08-22 06:49:22 +0900

    update

--- docker/docker_test.go
+++ docker/docker_test.go
@@ -5,6 +5,5 @@ import (
 )
 
 func TestDockerTokenBasic(t *testing.T) {
-	ret, code, token, err := RegistryAuthenticateBasic("jasonyangshadow/python", "pull", "jasonyangshadow", "jason294514")
-	t.Log(ret, code, token, err)
+
 }
