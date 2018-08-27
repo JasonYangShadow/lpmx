060575c5aca6fcf0079bdc709394e9e25736a603
jason <jasonyang.final@gmail.com>
2018-08-28 00:01:52 +0900

    update

--- docker/docker.go
+++ docker/docker.go
@@ -1,287 +1,127 @@
 package docker
 
 import (
-	b64 "encoding/base64"
-	"encoding/json"
 	"fmt"
+	"github.com/heroku/docker-registry-client/registry"
+	. "github.com/jasonyangshadow/lpmx/error"
+	. "github.com/jasonyangshadow/lpmx/utils"
 	"io"
 	"io/ioutil"
-	"net/http"
+	"log"
 	"os"
-	"time"
-
-	. "github.com/jasonyangshadow/lpmx/error"
+	"strings"
 )
 
 const (
-	DOCKER_HUB_TOKEN = "https://auth.docker.io/token?serivce=registry.docker.io&scope="
-	DOCKER_HUB_REQ   = "https://index.docker.io/v2/"
+	DOCKER_URL = "https://registry-1.docker.io"
 )
 
-func createTransport() *http.Transport {
-	tr := &http.Transport{
-		MaxIdleConns:       10,
-		IdleConnTimeout:    30 * time.Second,
-		DisableCompression: true,
-	}
-	return tr
-}
-
-func httpExCode(r *http.Response) (int, *Error) {
-	switch r.StatusCode {
-	case 200:
-		return 200, nil
-	case 401:
-		cerr := ErrNew(ErrHttpUnauthorized, "401 unauthorized")
-		return 401, cerr
-	case 404:
-		cerr := ErrNew(ErrHttpNotFound, "404 not found")
-		return 404, cerr
-	default:
-		bodyBytes, _ := ioutil.ReadAll(r.Body)
-		cerr := ErrNew(ErrUnknown, string(bodyBytes))
-		return -1, cerr
-	}
-}
-
-var http_client = &http.Client{Transport: createTransport(), Timeout: time.Second * 5}
-
-func V2Available() (bool, *Error) {
-	resp, err := http_client.Get(DOCKER_HUB_REQ)
-	defer resp.Body.Close()
+func ListRepositories(username string, pass string) ([]string, *Error) {
+	log.SetOutput(ioutil.Discard)
+	hub, err := registry.New(DOCKER_URL, username, pass)
 	if err != nil {
-		bodyBytes, _ := ioutil.ReadAll(resp.Body)
-		cerr := ErrNew(err, string(bodyBytes))
-		return false, cerr
+		cerr := ErrNew(err, "create docker registry instance failure")
+		return nil, cerr
 	}
-	retcode, cerr := httpExCode(resp)
-	if retcode == 200 {
-		return true, nil
+	repo, err := hub.Repositories()
+	if err != nil {
+		cerr := ErrNew(err, "query docker repositories failure")
+		return nil, cerr
 	}
-	return false, cerr
+	return repo, nil
 }
 
-//return params are: successful(bool), statuscode, token, err(if it has)
-func RegistryAuthenticate(name string, operations string) (bool, int, string, *Error) {
-	requrl := DOCKER_HUB_TOKEN + "repository:" + name + ":" + operations
-	resp, err := http_client.Get(requrl)
-	defer resp.Body.Close()
+func ListTags(username string, pass string, name string) ([]string, *Error) {
+	log.SetOutput(ioutil.Discard)
+	hub, err := registry.New(DOCKER_URL, username, pass)
 	if err != nil {
-		cerr := ErrNew(err, "http get encountered failure")
-		return false, -1, "", cerr
+		cerr := ErrNew(err, "create docker registry instance failure")
+		return nil, cerr
 	}
-	retcode, cerr := httpExCode(resp)
-	if retcode == 200 && cerr == nil {
-		bodyBytes, _ := ioutil.ReadAll(resp.Body)
-		if len(bodyBytes) == 0 {
-			cerr := ErrNew(ErrZero, "size of http return body is 0")
-			return false, -1, "", cerr
-		}
-		data := make(map[string]interface{})
-		if err := json.Unmarshal(bodyBytes, &data); err != nil {
-			cerr := ErrNew(err, "json unmarshal http body failure")
-			return false, -1, "", cerr
-		}
-		return true, 200, data["token"].(string), nil
-	} else {
-		cerr := ErrNew(err, "http get encountered failure")
-		bodyBytes, _ := ioutil.ReadAll(resp.Body)
-		return false, resp.StatusCode, string(bodyBytes), cerr
+	tags, err := hub.Tags(name)
+	if err != nil {
+		cerr := ErrNew(err, "query docker tags failure")
+		return nil, cerr
 	}
+	return tags, nil
 }
 
-func RegistryAuthenticateBasic(name string, operations string, user string, pass string) (bool, int, string, *Error) {
-	requrl := DOCKER_HUB_TOKEN + "repository:" + name + ":" + operations
-	req, _ := http.NewRequest("GET", requrl, nil)
-	auth := b64.StdEncoding.EncodeToString([]byte(user + ":" + pass))
-	req.Header.Add("Authorization", "Basic "+auth)
-	resp, err := http_client.Do(req)
-	defer resp.Body.Close()
+func GetDigest(username string, pass string, name string, tag string) (string, *Error) {
+	log.SetOutput(ioutil.Discard)
+	hub, err := registry.New(DOCKER_URL, username, pass)
 	if err != nil {
-		cerr := ErrNew(err, "http get encountered failure")
-		bodyBytes, _ := ioutil.ReadAll(resp.Body)
-		return false, -1, string(bodyBytes), cerr
+		cerr := ErrNew(err, "create docker registry instance failure")
+		return "", cerr
 	}
-	retcode, cerr := httpExCode(resp)
-	if retcode == 200 && cerr == nil {
-		bodyBytes, _ := ioutil.ReadAll(resp.Body)
-		if len(bodyBytes) == 0 {
-			cerr := ErrNew(ErrZero, "size of http return body is 0")
-			return false, -1, "", cerr
-		}
-		data := make(map[string]interface{})
-		if err := json.Unmarshal(bodyBytes, &data); err != nil {
-			cerr := ErrNew(err, "json unmarshal http body failure")
-			return false, -1, "", cerr
-		}
-		return true, 200, data["token"].(string), nil
-	} else {
-		cerr := ErrNew(err, "http get encountered failure")
-		bodyBytes, _ := ioutil.ReadAll(resp.Body)
-		return false, resp.StatusCode, string(bodyBytes), cerr
+	digest, err := hub.ManifestDigest(name, tag)
+	if err != nil {
+		cerr := ErrNew(err, "query docker digest failure")
+		return "", cerr
 	}
+	return digest.String(), nil
 }
 
-func PullManifest(name string, tag string, token string) *Error {
-	requrl := DOCKER_HUB_REQ + name + "/manifests/" + tag
-	req, _ := http.NewRequest("Get", requrl, nil)
-	req.Header.Add("Authorization", "Bearer "+token)
-	resp, err := http_client.Do(req)
-	fmt.Println(resp.Header)
-	bodyBytes, _ := ioutil.ReadAll(resp.Body)
-	defer resp.Body.Close()
+func DeleteManifest(username string, pass string, name string, tag string) *Error {
+	log.SetOutput(ioutil.Discard)
+	hub, err := registry.New(DOCKER_URL, username, pass)
 	if err != nil {
-		cerr := ErrNew(err, string(bodyBytes))
+		cerr := ErrNew(err, "create docker registry instance failure")
 		return cerr
 	}
-
-	fmt.Println(string(bodyBytes))
-	retcode, cerr := httpExCode(resp)
-	if retcode == 200 {
-		data := make(map[string]interface{})
-		if err := json.Unmarshal(bodyBytes, &data); err != nil {
-			cerr := ErrNew(err, "json unmarshal http body failure")
-			return cerr
-		}
-		fmt.Println(data)
-		return nil
-	}
-	return cerr
-}
-
-func CheckManifest(name string, tag string, token string) (bool, *Error) {
-	requrl := DOCKER_HUB_REQ + name + "/manifests/" + tag
-	req, _ := http.NewRequest("HEAD", requrl, nil)
-	req.Header.Add("Authorization", "Bearer "+token)
-	resp, err := http_client.Do(req)
-	defer resp.Body.Close()
+	digest, err := hub.ManifestDigest(name, tag)
 	if err != nil {
-		bodyBytes, _ := ioutil.ReadAll(resp.Body)
-		cerr := ErrNew(err, string(bodyBytes))
-		return false, cerr
-	}
-	retcode, cerr := httpExCode(resp)
-	if retcode == 200 {
-		return true, nil
-	}
-	return false, cerr
-}
-
-func CheckLayers(name string, digest string, token string) (bool, *Error) {
-	requrl := DOCKER_HUB_REQ + name + "/blobs/" + digest
-	req, _ := http.NewRequest("HEAD", requrl, nil)
-	req.Header.Add("Authorization", "Bearer "+token)
-	resp, err := http_client.Do(req)
-	defer resp.Body.Close()
-	if err != nil {
-		bodyBytes, _ := ioutil.ReadAll(resp.Body)
-		cerr := ErrNew(err, string(bodyBytes))
-		return false, cerr
-	}
-	retcode, cerr := httpExCode(resp)
-	if retcode == 200 {
-		return true, nil
-	}
-	return false, cerr
-}
-
-func PushLayer(name string, token string) *Error {
-	requrl := DOCKER_HUB_REQ + name + "/blobs/uploads/"
-	req, _ := http.NewRequest("POST", requrl, nil)
-	req.Header.Add("Authorization", "Bearer "+token)
-	resp, err := http_client.Do(req)
-	defer resp.Body.Close()
-	if err != nil {
-		bodyBytes, _ := ioutil.ReadAll(resp.Body)
-		cerr := ErrNew(err, string(bodyBytes))
+		cerr := ErrNew(err, "query docker digest failure")
 		return cerr
 	}
-	retcode, cerr := httpExCode(resp)
-	if retcode == 200 {
-		return nil
-	}
-	return cerr
-}
-
-func PullLayer(name string, digest string, path string, token string) *Error {
-	requrl := DOCKER_HUB_REQ + name + "/blobs/" + digest
-	req, _ := http.NewRequest("GET", requrl, nil)
-	req.Header.Add("Authorization", "Bearer "+token)
-	resp, err := http_client.Do(req)
-	defer resp.Body.Close()
+	err = hub.DeleteManifest(name, digest)
 	if err != nil {
-		bodyBytes, _ := ioutil.ReadAll(resp.Body)
-		cerr := ErrNew(err, string(bodyBytes))
+		cerr := ErrNew(err, "delete docker manifest failure")
 		return cerr
 	}
-	retcode, cerr := httpExCode(resp)
-	if retcode == 200 {
-		out, err := os.Create(path)
+	return nil
+}
+
+func DownloadLayers(username string, pass string, name string, tag string, folder string) *Error {
+	log.SetOutput(ioutil.Discard)
+	if !FolderExist(folder) {
+		_, err := MakeDir(folder)
 		if err != nil {
-			cerr := ErrNew(ErrUnknown, "create file error")
-			return cerr
+			return err
 		}
-		defer out.Close()
-		_, err = io.Copy(out, resp.Body)
+	}
+	hub, err := registry.New(DOCKER_URL, username, pass)
+	if err != nil {
+		cerr := ErrNew(err, "create docker registry instance failure")
+		return cerr
+	}
+	man, err := hub.ManifestV2(name, tag)
+	if err != nil {
+		cerr := ErrNew(err, "query docker manifest failure")
+		return cerr
+	}
+	for _, element := range man.Layers {
+		dig := element.Digest
+		reader, err := hub.DownloadLayer(name, dig)
 		if err != nil {
-			cerr := ErrNew(ErrUnknown, fmt.Sprintf("can't copy file from http to %s", path))
+			cerr := ErrNew(err, "download docker layers failure")
 			return cerr
 		}
-		return nil
+		defer reader.Close()
+		if strings.HasSuffix(folder, "/") {
+			folder = strings.TrimSuffix(folder, "/")
+		}
+		filename := folder + "/" + strings.TrimPrefix(dig.String(), "sha256:")
+		to, err := os.Create(filename)
+		if err != nil {
+			cerr := ErrNew(err, fmt.Sprintf("create file %s failure", filename))
+			return cerr
+		}
+		defer to.Close()
+		fmt.Println(fmt.Sprintf("Downloading file with type: %s, size: %d, destination: %s", element.MediaType, element.Size, filename))
+		if _, err := io.Copy(to, reader); err != nil {
+			cerr := ErrNew(err, fmt.Sprintf("copy file %s content failure", filename))
+			return cerr
+		}
 	}
-	return cerr
-}
-
-func ListRepositories(token string) (string, *Error) {
-	requrl := DOCKER_HUB_REQ + "_catalog"
-	req, _ := http.NewRequest("GET", requrl, nil)
-	req.Header.Add("Authorization", "Bearer "+token)
-	resp, err := http_client.Do(req)
-	defer resp.Body.Close()
-	bodyBytes, _ := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		cerr := ErrNew(err, string(bodyBytes))
-		return "", cerr
-	}
-	retcode, cerr := httpExCode(resp)
-	if retcode == 200 {
-		return string(bodyBytes), nil
-	}
-	return string(bodyBytes), cerr
-}
-
-func ListTags(name string, token string) (string, *Error) {
-	requrl := DOCKER_HUB_REQ + name + "/tags/list"
-	req, _ := http.NewRequest("GET", requrl, nil)
-	req.Header.Add("Authorization", "Bearer "+token)
-	resp, err := http_client.Do(req)
-	bodyBytes, _ := ioutil.ReadAll(resp.Body)
-	defer resp.Body.Close()
-	if err != nil {
-		cerr := ErrNew(err, string(bodyBytes))
-		return "", cerr
-	}
-	retcode, cerr := httpExCode(resp)
-	if retcode == 200 {
-		return string(bodyBytes), nil
-	}
-	return string(bodyBytes), cerr
-}
-
-func GetCatalog(token string) (string, *Error) {
-	requrl := DOCKER_HUB_REQ + "_catalog"
-	req, _ := http.NewRequest("GET", requrl, nil)
-	req.Header.Add("Authorization", "Bearer "+token)
-	resp, err := http_client.Do(req)
-	bodyBytes, _ := ioutil.ReadAll(resp.Body)
-	defer resp.Body.Close()
-	if err != nil {
-		cerr := ErrNew(err, string(bodyBytes))
-		return "", cerr
-	}
-	retcode, cerr := httpExCode(resp)
-	if retcode == 200 {
-		return string(bodyBytes), nil
-	}
-	return string(bodyBytes), cerr
+	return nil
 }
--- docker/docker_test.go
+++ docker/docker_test.go
@@ -4,32 +4,10 @@ import (
 	"testing"
 )
 
-var name = "library/alpine"
-var _, _, token, _ = RegistryAuthenticate(name, "pull")
-
 func TestAuthentication(t *testing.T) {
-	t.Skip("skip test")
-	ret, code, token, err := RegistryAuthenticate(name, "pull")
-	t.Log(ret, code, token, err)
-}
-
-func TestGetCatalog(t *testing.T) {
-	t.Skip("skip test")
-	t.Log(GetCatalog(token))
-}
-
-func TestPullManifest(t *testing.T) {
 	//t.Skip("skip test")
-	t.Log(token)
-	t.Log(PullManifest(name, "latest", token))
-}
-
-func TestDockerV2(t *testing.T) {
-	t.Skip("skip test")
-	t.Log(V2Available())
-}
-
-func TestListTags(t *testing.T) {
-	t.Skip("skip test")
-	t.Log(ListTags(name, token))
+	err := DownloadLayers("", "", "library/ubuntu", "latest", "/tmp")
+	if err != nil {
+		t.Error(err)
+	}
 }
