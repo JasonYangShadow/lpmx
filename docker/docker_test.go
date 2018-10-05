5bae7ab3fe7ff0033ddcf423501bdf00fae31c42
jason <jasonyang.final@gmail.com>
2018-10-05 11:16:03 +0900

    update

--- docker/docker_test.go
+++ docker/docker_test.go
@@ -9,8 +9,8 @@ import (
 func TestDownloadLayers(t *testing.T) {
 	//t.Skip("skip test")
 	url := "https://registry-1.docker.io/"
-	username := "jasonyangshadow" // anonymous
-	password := "jason294514"     // anonymous
+	username := "" // anonymous
+	password := "" // anonymous
 	hub, _ := registry.New(url, username, password)
 	manifest, err := hub.ManifestV2("/ubuntu", "latest")
 	if err != nil {
