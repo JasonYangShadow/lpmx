35437bceaf8a3c0ce2a9af44e98fd2834e77c30e
jason <jasonyang.final@gmail.com>
2018-12-05 14:49:02 +0000

    update

--- container/container.go
+++ container/container.go
@@ -969,7 +969,7 @@ func (con *Container) genEnv() (map[string]string, *Error) {
 	}
 
 	//set default FAKECHROOT_EXCLUDE_PATH
-	env["FAKECHROOT_EXCLUDE_PATH"] = "/dev:/proc:/sys"
+	env["FAKECHROOT_EXCLUDE_PATH"] = "/tmp:/dev:/proc:/sys"
 
 	//set default FAKECHROOT_CMD_SUBSET
 	env["FAKECHROOT_CMD_SUBSET"] = "/sbin/ldconfig=/bin/true:/usr/bin/ldd=/usr/bin/ldd:/dev/null=/bin/true"
