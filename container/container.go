4c1ca53ce0c4e71f29397091fe7c78fd3d184d41
jason <jasonyang.final@gmail.com>
2018-12-24 17:56:53 +0900

    update

--- container/container.go
+++ container/container.go
@@ -982,7 +982,7 @@ func (con *Container) genEnv() (map[string]string, *Error) {
 	env["FAKECHROOT_EXCLUDE_PATH"] = "/tmp:/dev:/proc:/sys"
 
 	//set default FAKECHROOT_CMD_SUBSET
-	env["FAKECHROOT_CMD_SUBSET"] = "/sbin/ldconfig.real=/bin/true:/sbin/insserv=/bin/true:/sbin/ldconfig=/bin/ldconfig:/usr/bin/ischroot=/bin/true:/usr/bin/mkfifo=/bin/true"
+	env["FAKECHROOT_CMD_SUBST"] = "/sbin/ldconfig.real=/bin/true:/sbin/insserv=/bin/true:/sbin/ldconfig=/bin/ldconfig:/usr/bin/ischroot=/bin/true:/usr/bin/mkfifo=/bin/true"
 
 	env["HOME"], _ = GuessPathContainer(filepath.Dir(con.RootPath), strings.Split(con.Layers, ":"), "home", false)
 
